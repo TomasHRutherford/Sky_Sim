5fc02397147bdfdd63f6f0aae2059435d816e043
TomasHRutherford <trut2989@uni.sydney.edu.au>
2023-03-22 11:49:08 +1100

    multi_processing

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -1,94 +1,90 @@
+#! /usr/bin/env python
+# Demonstrate that we can simulate a catalogue of stars on the sky
+
 # Determine Andromeda location in ra/dec degrees
-import random
 import math
-import argparse
-# from wikipedia
-ra = '00:42:44.3'
-dec = '41:16:09'
+import numpy as np
+import multiprocessing
+import sys
+
 NSRC = 1_000_000
 
-def skysim_parser():
-    """
-    Configure the argparse for skysim
-
-    Returns
-    -------
-    parser : argparse.ArgumentParser
-        The parser for skysim.
-    """
-    parser = argparse.ArgumentParser(prog='sky_sim', prefix_chars='-')
-    parser.add_argument('--ra', dest = 'ra', type=float, default=None,
-                        help="Central ra (degrees) for the simulation location")
-    parser.add_argument('--dec', dest = 'dec', type=float, default=None,
-                        help="Central dec (degrees) for the simulation location")
-    parser.add_argument('--out', dest='out', type=str, default='catalog.csv',
-                        help='destination for the output catalog')
-    return parser
-
-def gen_pos(NSRC, ra, dec):
-    """
-    Generates the positions of stars around the centre of Andromeda,
-    with a maximum of 1 degree offset in x and y.
-    """
-    ras = []
-    decs = []
-    for i in range(NSRC):
-        ras.append(ra + random.uniform(-1,1))
-        decs.append(dec + random.uniform(-1,1))
-    return ras, decs
-
-def save_file(ras, decs):
-    """
-    Saves our star positions as a csv file.
-    """
-    f = open('catalog.csv','w')
-    print("id,ra,dec", file=f)
-    for i in range(NSRC):
-        print("{0:07d}, {1:12f}, {2:12f}".format(i, ras[i], decs[i]), file=f)
 
 def get_radec():
-    """
-    Generate the ra/dec coordinates of Andromeda
-    in decimal degrees.  
-
-    Hours/degrees/seconds values are taken from wikipedia.
-
-    Returns
-    -------
-    ra:float  
-    &emsp;    The position of the centre of Andromeda on the sky in Right Ascension, given in degrees.  
-    dec:float  
-    &emsp;    The position of the centre of Andromeda on the sky in Declination, given in degrees.  
-    """
     # from wikipedia
-    andromeda_ra = '00:42:44.3'
-    andromeda_dec = '41:16:09'
+    ra = '00:42:44.3'
+    dec = '41:16:09'
 
-    d, m, s = andromeda_dec.split(':')
+    d, m, s = dec.split(':')
     dec = int(d)+int(m)/60+float(s)/3600
 
-    h, m, s = andromeda_ra.split(':')
+    h, m, s = ra.split(':')
     ra = 15*(int(h)+int(m)/60+float(s)/3600)
     ra = ra/math.cos(dec*math.pi/180)
     return ra,dec
 
-if __name__ == "__main__":
-    parser = skysim_parser()
-    options = parser.parse_args()
-    # if ra/dec are not supplied the use a default value
-    if None in [options.ra, options.dec]:
-        ra, dec = get_radec()
-    else:
-        ra = options.ra
-        dec = options.dec
+
+def make_stars(args):
+    """
+    """
+    #unpack the arguments
+    ra, dec, nsrc = args
+    # create an empy array for our results
+    radec = np.empty((2,nsrc))
+
+    # make nsrc stars within 1 degree of ra/dec
+    radec[0,:] = np.random.uniform(ra-1, ra+1, size=nsrc)
+    radec[1,:] = np.random.uniform(dec-1, dec+1, size=nsrc)
     
-    ras, decs = gen_pos(NSRC,ra,dec)
+    # return our results
+    return radec
+
+def make_stars_parallel(ra, dec, nsrc=NSRC, cores=None):
+    
+    # By default use all available cores
+    if cores is None:
+        cores = multiprocessing.cpu_count()
+    
+
+    # 20 jobs each doing 1/20th of the sources
+    group_size = nsrc//20
+    args = [(ra, dec, group_size) for _ in range(20)]
+
+
+    # start a new process for each task, hopefully to reduce residual
+    # memory use
+    ctx = multiprocessing.get_context()
+    pool = ctx.Pool(processes=cores, maxtasksperchild=1)
+
+    try:
+        # call make_posisions(a) for each a in args
+        results = pool.map(make_stars, args, chunksize=1)
+    except KeyboardInterrupt:
+        # stop all the processes if the user calls the kbd interrupt
+        print("Caught kbd interrupt")
+        pool.close()
+        sys.exit(1)
+    else:
+        # join the pool means wait until there are results
+        pool.close()
+        pool.join()
+
+        # crete an empty array to hold our results
+        radec = np.empty((2,nsrc),dtype=np.float64)
+
+        # iterate over the results (a list of whatever was returned from make_stars)
+        for i,r in enumerate(results):
+            # store the returned results in the right place in our array
+            start = i*group_size
+            end = start + group_size
+            radec[:,start:end] = r
+            
+    return radec
+
+if __name__ == "__main__":
+    ra,dec = get_radec()
+    pos = make_stars_parallel(ra, dec, NSRC, 2)
     # now write these to a csv file for use by my other program
-    with open(options.out,'w') as f:
+    with open('catalog.csv', 'w') as f:
         print("id,ra,dec", file=f)
-        for i in range(NSRC):
-            print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
-    print(f"Wrote {options.out}")
-
-
-
+        np.savetxt(f, np.column_stack((np.arange(NSRC), pos[0,:].T, pos[1,:].T)),fmt='%07d, %12f, > %12f')
